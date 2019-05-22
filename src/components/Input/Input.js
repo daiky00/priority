772a005c7134d710727a145dd701f701954608d3
Francisco Pena <project.01@hotmail.com>
2019-05-22 16:08:11 -0500

    added Input Component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from './Input.module.css';
+
+const Size = (size) => {
+  switch(size) {
+    case 'sm':
+     return styles.sm
+    case 'md':
+     return styles.md
+    case 'lg':
+     return styles.lg
+    case 'xl':
+     return styles.xl
+    default:
+      return size
+  }
+}
+
+const Input = (props) => {
+  return (
+    <React.Fragment>
+      <label className={`${styles.label}`} htmlFor={props.name}>{props.title}</label>
+      <input 
+        className={`${styles.input} ${Size(props.size)}`}
+        id={props.name}
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        onChange={props.handleChange}
+        placeholder={props.placeholder}
+        required={props.required}
+      />
+      <span className={`${styles.error}`}>{props.error}</span>
+
+    </React.Fragment>
+  )
+}
+
+export default Input;
