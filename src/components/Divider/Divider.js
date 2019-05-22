18e8d2dbb3c73f2dd26ebc175ddd2bc60f9497fc
Francisco Pena <project.01@hotmail.com>
2019-05-22 16:09:10 -0500

    added Divider Component

---
+++ src/components/Divider/Divider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Divider.module.css'
+
+const Divider = () => {
+  return (
+    <hr className={styles.divider} />
+  )
+}
+
+export default Divider;
